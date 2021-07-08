edd0a2fd4331c948132017d2826692558a3d8f85
tensorturtle <tensorturtle@gmail.com>
2021-07-09 05:17:14 +0900

    add tests for bbox

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,19 @@
 import unittest
 
 from bboxconvert import test_sum
+from bboxconvert.bboxconvert import coco_to_voc
 
 class TestSum(unittest.TestCase):
     def test_list_int(self):
         data = [1,2,3]
         result = test_sum(data)
         self.assertEqual(result, 6)
+
+class BboxTest(unittest.TestCase):
+    def test_coordinate_conversions(self):
+        data = [1,1,1,1]
+        result = coco_to_voc(data)
+        self.asserEqual(result, [1,1,2,2])
 
 if __name__=='__main__':
     unittest.main()
