2a0536e895b2b91f5bcb4deb1905ca07eacf1e67
tensorturtle <tensorturtle@gmail.com>
2021-07-09 03:36:23 +0900

    test pypi upload

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="bboxconvert",
-    version="0.2.0",
+    version="0.2.1",
     description="Convert between bounding box annotation formats",
     long_description=README,
     long_description_content_type="text/markdown",
