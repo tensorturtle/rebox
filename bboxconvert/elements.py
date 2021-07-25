d348eb06becda4e14d3cd6f0e8a0f3528e794b2d
tensorturtle <tensorturtle@gmail.com>
2021-07-26 03:54:41 +0900

    initial commit

--- bboxconvert/elements.py
+++ bboxconvert/elements.py
@@ -1,4 +1,4 @@
-def xCyCwh_to_xyXY(coords):
+def XcYcWH_to_XYXY(coords):
     '''
     [x_center, y_center, width, height]
     to
@@ -16,7 +16,7 @@ def xCyCwh_to_xyXY(coords):
 
     return [x_min, y_min, x_max, y_max]
 
-def xyXY_to_xCyCwh(coords):
+def XYXY_to_XcYcWH(coords):
     '''
     [x_min, y_min, x_max, y_max]
     to
@@ -27,14 +27,14 @@ def xyXY_to_xCyCwh(coords):
     x_max = coords[2]
     y_max = coords[3]
 
-    x_center = (x_min + x_max) / 2 
+    x_center = (x_min + x_max) / 2
     y_center = (y_min + y_max) / 2
     width = x_max - x_min
     height = y_max - y_min
 
     return [x_center, y_center, width, height]
 
-def xywh_to_xyXY(coords):
+def XmYmWH_to_XYXY(coords):
     x_min = coords[0]
     y_min = coords[1]
     width = coords[2]
@@ -45,7 +45,7 @@ def xywh_to_xyXY(coords):
 
     return [x_min, y_min, x_max, y_max]
 
-def xyXY_to_xywh(coords):
+def XYXY_to_XmYmWH(coords):
     x_min = coords[0]
     y_min = coords[1]
     x_max = coords[2]
@@ -69,5 +69,76 @@ def denormalize(coords, width, height):
     Assuming [x,y,x,y] values
     '''
     return [coords[0]*width, coords[1]*height, coords[2]*width, coords[3]*height]
-    
 
+def pixel_XmYmWH_to_XYXY(coords):
+    x_min = coords[0]
+    y_min = coords[1]
+    width = coords[2]
+    height = coords[3]
+
+    x_max = x_min + width - 1 # accounting for fencepost error; pixel bounding box width/height is taken to be the 'outside' measure
+    y_max = y_min + height - 1
+    return [x_min, y_min, x_max, y_max]
+
+def pixel_XcYcWH_to_XYXY(coords):
+    x_center = coords[0]
+    y_center = coords[1]
+    width = coords[2]
+    height = coords[3]
+
+    exact_width = width - 1 # accounting for fencepost error; pixel bounding box width/height is taken to be the 'outside' measure
+    exact_height = height - 1
+
+    x_min = x_center - exact_width/2
+    x_max = x_center + exact_width/2
+    y_min = y_center - exact_height/2
+    y_max = y_center + exact_height/2
+
+    return [x_min, y_min, x_max, y_max]
+
+def pixel_XYXY_to_XmYmWH(coords):
+    x_min = coords[0]
+    y_min = coords[1]
+    x_max = coords[2]
+    y_max = coords[3]
+
+    exact_width = x_max - x_min
+    fencepost_width = exact_width + 1
+
+    exact_height = y_max - y_min
+    fencepost_height = exact_height + 1
+
+    return [x_min, y_min, fencepost_width, fencepost_height]
+
+def pixel_XYXY_to_XcYcWH(coords):
+    x_min = coords[0]
+    y_min = coords[1]
+    x_max = coords[2]
+    y_max = coords[3]
+
+    x_center = (x_max - x_min) / 2
+    y_center = (y_max - y_min) / 2
+
+    exact_width = x_max - x_min
+    fencepost_width = exact_width + 1
+
+    exact_height = y_max - y_min
+    fencepost_height = exact_height + 1
+
+    return [x_center, y_center, fencepost_width, fencepost_height]
+
+def xyxy_scaled_rel_to_abs(coords, scale, width, height):
+    x_min = coords[0]
+    y_min = coords[1]
+    x_max = coords[2]
+    y_max = coords[3]
+
+    abs_x_min = x_min / scale * width
+    abs_y_min = y_min / scale * height
+    abs_x_max = x_max / scale * width
+    abs_y_max = y_max / scale * height
+
+    return [abs_x_min, abs_y_min, abs_x_max, abs_y_max]
+
+def xyxy_abs_to_scaled_rel(coords, scale, width, height):
+    return [coords[0]/width, coords[1]/height, coords[2]/width, coords[3]/height]
