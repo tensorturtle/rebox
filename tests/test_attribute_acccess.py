601acb814612be057a9dcd0faa24e67ae6537e96
tensorturtle <tensorturtle@gmail.com>
2021-07-28 03:09:53 +0900

    fix test imports

--- tests/test_attribute_acccess.py
+++ tests/test_attribute_acccess.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 from rebox import BBox
-from rebox.common_formats import coco, yolo, pascal, albumentations, label_studio
+from rebox.formats import coco, yolo, pascal, albumentations, label_studio
 
 # coco example input
 @pytest.fixture
