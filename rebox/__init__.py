3f508221867aa90a2c549e662d2f534ca532bacc
tensorturtle <tensorturtle@gmail.com>
2021-07-27 13:14:36 +0900

    put init package back

--- rebox/__init__.py
+++ rebox/__init__.py
@@ -1,3 +1,9 @@
+"""Main package initialization."""
+
+from rebox.bbox import BBox2D
+from rebox.format import BBoxFormat
+from rebox.common_formats import yolo, coco, pascal, albumentations, label_studio
+
 __author__ = "Jason Sohn"
 __copyright__ = "Jason Sohn, 2021"
 __version__ = "0.1.0"
