556bb55338e98534a676acea23830c970ff200f6
tensorturtle <tensorturtle@gmail.com>
2021-07-27 12:41:22 +0900

    first real test in tests/test_attribute_access

---
+++ tests/test_attribute_acccess.py
@@ -0,0 +1,12 @@
+import pytest
+import numpy as np
+from rebox import BBox2D
+from rebox.common_formats import coco, yolo, pascal, albumentations, label_studio
+
+# coco example input
+@pytest.fixture
+def coco_example():
+    return BBox2D([10,20,30,30],coco)
+
+def test_value(coco_example):
+    assert np.array_equal(coco_example.value, np.array([10,20,30,30]))
