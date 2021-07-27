6c308b0359bd8702fe8d312f6e1503008af7ba18
tensorturtle <tensorturtle@gmail.com>
2021-07-28 07:04:13 +0900

    implement bboxformat-aware IoU

--- readme_examples/iou.py
+++ readme_examples/iou.py
@@ -1,8 +1,8 @@
 from rebox import BBox
 from rebox.formats import coco, pascal
-from rebox.ops import IOU
+from rebox.ops import iou
 
 one_bbox = BBox([40,50,20,10], coco)
 two_bbox = BBox([45,60, 30, 20], pascal)
 
-iou = IOU(one_bbox, two_bbox)
+iou = iou(one_bbox, two_bbox)
--- rebox/ops.py
+++ rebox/ops.py
@@ -1,2 +1,66 @@
-def IOU(one, two):
-    pass
+import numpy as np
+from rebox import BBox
+from rebox import BBoxFormat
+
+def iou(a, b):
+    """
+    Compute the Jaccarrd Index / Intersection over Union of a pair of 2D bounding boxes.
+
+    Args:
+        a: BBox object
+        b: BBox object
+
+    Returns:
+        The IoU of the two bounding boxes.
+    """
+
+    # dealing with various formats:
+    # unify style to XYXY,
+    # keep scale, and account for it in algorithm
+
+    assert a.format.style != b.format.style, "IoU calculation not implemented for bounding boxes of different formats. Please convert the two BBoxes to the same format before calculating iou()."
+
+    scale = a.format.scale
+    style = a.format.style
+
+    unified_format = BBoxFormat(style, scale)
+
+    a = a.to_format(unified_format)
+    b = b.to_format(unified_format)
+
+    xA = np.maximum(a.x1, b.x1)
+    yA = np.maximum(a.y1, b.y1)
+    xB = np.maximum(a.x2, b.x2)
+    yB = np.maximum(a.y2, b.y2)
+
+    if scale is None:
+        inter_w = xB - xA + 1
+    else:
+        inter_w = xB - xA
+
+    inter_w = inter_w * (inter_w >= 0)
+
+    if scale is None:
+        inter_h = yB - yA + 1
+    else:
+        inter_h = yB - yA
+
+    inter_h = inter_h * (inter_h >= 0)
+
+    intersection = inter_w * inter_h
+
+    a_area = a.width * a.height
+    b_area = b.width * b.height
+
+    iou = intersection / (a_area + b_area - intersection)
+
+    if np.isinf(iou) or np.isnan(iou):
+        iou = 0
+
+    return iou
+
+
+
+
+
+
