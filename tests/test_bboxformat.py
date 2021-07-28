1f835b8c92b10be108bfbc4ba2832f0d63cedfc7
tensorturtle <tensorturtle@gmail.com>
2021-07-28 09:41:06 +0900

    working on bboxformat test

---
+++ tests/test_bboxformat.py
@@ -0,0 +1,12 @@
+import pytest
+import numpy as np
+from rebox import BBoxFormat
+from rebox.formats import coco, yolo, pascal, albumentations, label_studio
+
+style_xyxy = "XYXY"
+style_xmymwh = "XmYmWH"
+
+# coco example input
+@pytest.fixture
+def coco_example():
+    return BBox([10,20,30,30],coco)
