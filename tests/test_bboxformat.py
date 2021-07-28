e8b52847cefa218b09bd2bba4c79ca3378c24bf2
tensorturtle <tensorturtle@gmail.com>
2021-07-29 00:45:42 +0900

    fix typo

--- tests/test_bboxformat.py
+++ tests/test_bboxformat.py
@@ -6,23 +6,23 @@ def example_xyxy_1000_format():
     return BBoxFormat("XYXY", 1000)
 
 def test_style(example_xyxy_1000_format):
-    assert example_format.style == "XYXY"
+    assert example_xyxy_1000_format.style ="XYXY"
 
 def test_scale(example_xyxy_1000_format):
-    assert example_format.scale == 1000
+    assert example_xyxy_1000_format.scale == 1000
 
 def test_scale(example_xyxy_1000_format):
-    assert example_format.is_relative == True
+    assert example_xyxy_1000_format.is_relative == True
 
 @pytest.fixture
 def example_xmymwh_none_format():
     return BBoxFormat("XmYmWH", None)
 
 def test_style(example_xmymwh_none_format):
-    assert example_format.style == "XmYmWH"
+    assert example_xmymwh_none_format.style == "XmYmWH"
 
 def test_scale(example_xyxy_1000_format):
-    assert example_format.scale == None
+    assert example_xmymwh_none_format.scale == None
 
 def test_scale(example_xyxy_1000_format):
-    assert example_format.is_relative == False
+    assert example_xmymwh_none_format.is_relative == False
