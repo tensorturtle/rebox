de43f6bbae86f80ba3989354e5b6ad7f19455ef0
tensorturtle <tensorturtle@gmail.com>
2021-07-29 01:06:44 +0900

    bboxformat works

--- tests/test_bboxformat.py
+++ tests/test_bboxformat.py
@@ -2,27 +2,14 @@ import pytest
 from rebox import BBoxFormat
 
 @pytest.fixture
-def example_xyxy_1000_format():
-    return BBoxFormat("XYXY", 1000)
+def example_format():
+    return BBoxFormat("XYXY", 10)
 
-def test_style(example_xyxy_1000_format):
-    assert example_xyxy_1000_format.style ="XYXY"
+def test_style(example_format):
+    assert example_format.style == "XYXY"
 
-def test_scale(example_xyxy_1000_format):
-    assert example_xyxy_1000_format.scale == 1000
+def test_scale(example_format):
+    assert example_format.scale == 10
 
-def test_scale(example_xyxy_1000_format):
-    assert example_xyxy_1000_format.is_relative == True
-
-@pytest.fixture
-def example_xmymwh_none_format():
-    return BBoxFormat("XmYmWH", None)
-
-def test_style(example_xmymwh_none_format):
-    assert example_xmymwh_none_format.style == "XmYmWH"
-
-def test_scale(example_xyxy_1000_format):
-    assert example_xmymwh_none_format.scale == None
-
-def test_scale(example_xyxy_1000_format):
-    assert example_xmymwh_none_format.is_relative == False
+def test_is_relative(example_format):
+    assert example_format.is_relative == True
