c50678143c5c68c58762a9b495167e50b7d950f0
tensorturtle <tensorturtle@gmail.com>
2021-07-29 00:27:32 +0900

    fix bboxformat test

--- tests/test_bboxformat.py
+++ tests/test_bboxformat.py
@@ -8,17 +8,12 @@ style_xyxy = "XYXY"
 style_xmymwh = "XmYmWH"
 style_xcycwh = "XcYcWH"
 
-# coco example input
-@pytest.fixture
-def coco_example():
-    return BBox([10,20,30,30],coco)
+def test_style_attribute():
+    assert coco.style == style_xmymwh
 
-def test_style_attribute(coco_example):
-    assert coco_example.style == style_xmymwh
+def test_scale_attribute():
+    assert coco.scale == None
 
-def test_scale_attribute(coco_example):
-    assert coco_example.scale == None
-
-def test_is_relative_attribute(coco_example):
-    assert coco_example.is_relative == True
+def test_is_relative_attribute():
+    assert coco.is_relative == True
 
